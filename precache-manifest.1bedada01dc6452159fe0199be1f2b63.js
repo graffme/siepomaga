e0d0a6b1e310575a9f600b5b76f2e3bb359d5038
graffme <daria.rucka@gmail.com>
2019-01-17 22:50:58 +0100

    Updates

---
+++ precache-manifest.1bedada01dc6452159fe0199be1f2b63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "77b6fd29a63dbd04b1eabd514dc214f3",
+    "url": "/siepomaga/static/media/heart.77b6fd29.png"
+  },
+  {
+    "revision": "0043b12c4fb200a57b89",
+    "url": "/siepomaga/static/js/runtime~main.0043b12c.js"
+  },
+  {
+    "revision": "681c1a0bec5bdc1e60a2",
+    "url": "/siepomaga/static/js/main.681c1a0b.chunk.js"
+  },
+  {
+    "revision": "7384ae4e1f741212bbfe",
+    "url": "/siepomaga/static/js/1.7384ae4e.chunk.js"
+  },
+  {
+    "revision": "681c1a0bec5bdc1e60a2",
+    "url": "/siepomaga/static/css/main.8c31174a.chunk.css"
+  },
+  {
+    "revision": "7384ae4e1f741212bbfe",
+    "url": "/siepomaga/static/css/1.7347b5f7.chunk.css"
+  },
+  {
+    "revision": "c3c9324cef6b12ba1468234ff510324f",
+    "url": "/siepomaga/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/siepomaga/precache-manifest.ae2c8be4dd6a1d1290b6e672f02f0208.js"
+  "/siepomaga/precache-manifest.1bedada01dc6452159fe0199be1f2b63.js"
 );
 
 workbox.clientsClaim();
