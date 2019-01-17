248ec89549291a161d61cebbe78624b198ba763e
graffme <daria.rucka@gmail.com>
2019-01-17 19:41:36 +0100

    Updates

---
+++ precache-manifest.ae2c8be4dd6a1d1290b6e672f02f0208.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "77b6fd29a63dbd04b1eabd514dc214f3",
+    "url": "/siepomaga/static/media/heart.77b6fd29.png"
+  },
+  {
+    "revision": "0043b12c4fb200a57b89",
+    "url": "/siepomaga/static/js/runtime~main.0043b12c.js"
+  },
+  {
+    "revision": "710d7ca8c1d0d6ccfc0e",
+    "url": "/siepomaga/static/js/main.710d7ca8.chunk.js"
+  },
+  {
+    "revision": "4252d7ef823ed5ef1a06",
+    "url": "/siepomaga/static/js/1.4252d7ef.chunk.js"
+  },
+  {
+    "revision": "710d7ca8c1d0d6ccfc0e",
+    "url": "/siepomaga/static/css/main.85f148c0.chunk.css"
+  },
+  {
+    "revision": "4252d7ef823ed5ef1a06",
+    "url": "/siepomaga/static/css/1.637b2074.chunk.css"
+  },
+  {
+    "revision": "7dfbb29756c2bbb74b760d8e9c14ad5e",
+    "url": "/siepomaga/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/siepomaga/precache-manifest.ae2c8be4dd6a1d1290b6e672f02f0208.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/siepomaga/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
