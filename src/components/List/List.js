019782ad58ea484c58c1bda31c6c5ecd1de53d16
graffme <daria.rucka@gmail.com>
2019-01-18 00:32:27 +0100

    Add loadMore function for List component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../../styles/styles.scss';
 import styles from './List.module.scss';
 import ListItem from '../ListItem/ListItem.js';
 import StandardButton from '../StandardButton/StandardButton.js';
 
-const List = (props) => {
-  const payments = props.payments;
-  const itemsList = payments.map((payment) =>
-    <ListItem  amount={payment.amount} className="tile bottom30" comment_text={payment.comment_text} paid_at={payment.paid_at} signature={payment.signature} />
-  );
+class List extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <section className={styles.list}>
-      <h2 className="bottom20">Wsparli</h2>
-      {itemsList}
-      <StandardButton label="Pokaż więcej" onClick={() => console.log('Clicked')} />
-    </section>
-  );
+    this.state = {
+      visible: 5,
+    };
+  }
+
+  loadMore = () => {
+    this.setState((prev) => {
+      return {visible: prev.visible + 5};
+    });
+  }
+
+  render() {
+    const { payments } = this.props;
+    const { visible } = this.state;
+    const itemsList = payments.slice(0, visible).map((payment) =>
+      <ListItem key={payment.id} amount={payment.amount} className="tile bottom30" comment_text={payment.comment_text} paid_at={payment.paid_at} signature={payment.signature} />
+    );
+
+    return (
+      <section className={styles.list}>
+        <h2 className="bottom20">Wsparli</h2>
+        {itemsList}
+
+        {visible < 20 &&
+          <StandardButton label="Pokaż więcej" onClick={() => this.loadMore()} />
+        }
+      </section>
+    );
+  }
 }
 
 export default List;
