a1266f43aff9131d86893819d9b7d32f7fa28710
graffme <daria.rucka@gmail.com>
2019-01-18 00:33:18 +0100

    Updates

--- precache-manifest.25b3f1ab807c1021ee0824ca86bf6fa2.js
+++ precache-manifest.25b3f1ab807c1021ee0824ca86bf6fa2.js
@@ -8,23 +8,23 @@ self.__precacheManifest = [
     "url": "/siepomaga/static/js/runtime~main.0043b12c.js"
   },
   {
-    "revision": "681c1a0bec5bdc1e60a2",
-    "url": "/siepomaga/static/js/main.681c1a0b.chunk.js"
+    "revision": "986ee907baf9201133e3",
+    "url": "/siepomaga/static/js/main.986ee907.chunk.js"
   },
   {
-    "revision": "7384ae4e1f741212bbfe",
-    "url": "/siepomaga/static/js/1.7384ae4e.chunk.js"
+    "revision": "ff5b81b3d13603209fe8",
+    "url": "/siepomaga/static/js/1.ff5b81b3.chunk.js"
   },
   {
-    "revision": "681c1a0bec5bdc1e60a2",
-    "url": "/siepomaga/static/css/main.8c31174a.chunk.css"
+    "revision": "986ee907baf9201133e3",
+    "url": "/siepomaga/static/css/main.4531a09e.chunk.css"
   },
   {
-    "revision": "7384ae4e1f741212bbfe",
+    "revision": "ff5b81b3d13603209fe8",
     "url": "/siepomaga/static/css/1.7347b5f7.chunk.css"
   },
   {
-    "revision": "c3c9324cef6b12ba1468234ff510324f",
+    "revision": "2f38da417935aa389475a6c5e6784d20",
     "url": "/siepomaga/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/siepomaga/precache-manifest.1bedada01dc6452159fe0199be1f2b63.js"
+  "/siepomaga/precache-manifest.25b3f1ab807c1021ee0824ca86bf6fa2.js"
 );
 
 workbox.clientsClaim();
